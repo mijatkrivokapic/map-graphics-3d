1f408687172afb7d1a1a2736083f06cfb893c033
Mijat Krivokapić <mijat2004@gmail.com>
2026-02-11 13:16:07 +0100

    Implement measuring mode

--- Kostur/Source/Main.cpp
+++ Kostur/Source/Main.cpp
@@ -42,27 +42,52 @@ float playerRotation = 180.0f;
 float totalDistance = 0.0f; // Ukupna predjena razdaljina
 glm::vec3 lastPlayerPos = glm::vec3(0.0f, 0.0f, 0.0f);
 
+std::vector<glm::vec3> measurementPoints; // Niz tačaka (čioda)
+float measurementTotalLen = 0.0f;         // Ukupna dužina
+
+// Za iscrtavanje linija
+unsigned int lineVAO, lineVBO;
+
 // Deklaracije funkcija
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 void renderMap(unsigned int vao, Shader& shader, unsigned int texture);
+void mouse_callback(GLFWwindow* window, int button, int action, int mods);
 
 int main()
 {
-    // 1. INICIJALIZACIJA GLFW i GLEW (Isto kao pre)
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "3D Mapa Projekat", NULL, NULL);
+    // --- FULL SCREEN KOD ---
+
+    // 1. Pronadji primarni monitor
+    GLFWmonitor* primaryMonitor = glfwGetPrimaryMonitor();
+
+    // 2. Pronadji trenutnu rezoluciju tog monitora (npr. 1920x1080 @ 60Hz)
+    const GLFWvidmode* mode = glfwGetVideoMode(primaryMonitor);
+
+    // 3. Kreiraj prozor koristeci te dimenzije
+    // Cetvrti parametar 'primaryMonitor' znaci: "Otvori u Full Screen-u na ovom monitoru"
+    GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "3D Mapa Projekat", primaryMonitor, NULL);
+
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
+
+    // Azuriramo viewport da se slaze sa novom rezolucijom
+    glViewport(0, 0, mode->width, mode->height);
+
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+    glfwSetMouseButtonCallback(window, mouse_callback);
 
     if (glewInit() != GLEW_OK) {
         std::cout << "Failed to initialize GLEW" << std::endl;
@@ -80,7 +105,7 @@ int main()
     // 3. UČITAVANJE MODELA
     // Putanje prilagodi svojim fajlovima
     Model humanoidModel("Resources/bob-model/bob_the_builder.obj"); // Tvoj lik
-    Model pinModel("Resources/low-poly-fox.obj");        // Tvoja čioda
+    Model pinModel("Resources/pin-model/map_pin.obj");        // Tvoja čioda
 
     // 4. GENERISANJE MAPE (Pravougaonik u XZ ravni)
     // Mapa leži na Y=0, proteže se od -10 do 10
@@ -117,6 +142,16 @@ int main()
     // Učitaj teksturu mape (koristeći tvoju ili bibliotečku funkciju)
     unsigned int mapTexture = TextureFromFile("map.jpg", "Resources");
 
+    // --- SETUP ZA LINIJE ---
+    glGenVertexArrays(1, &lineVAO);
+    glGenBuffers(1, &lineVBO);
+    glBindVertexArray(lineVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
+    // Samo nam trebaju pozicije (X, Y, Z)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
     // Inicijalizacija tvog tekst sistema
     initText(uiShader.ID, "Resources/Antonio-Regular.ttf");
 
@@ -150,28 +185,9 @@ int main()
         phongShader.setVec3("uViewPos", cameraPos);
 
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
-        if (isWalkingMode) {
-            // Kamera prati igraca:
-            // X i Z su isti kao igrac (ili malo iza), Y je visoko iznad
-            // Namestamo da kamera bude 4 jedinice IZA (po Z) i 3 jedinice IZNAD (po Y)
-            view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
-
-            float dist = glm::distance(playerPos, lastPlayerPos);
-
-            // Dodaj na ukupnu sumu
-            totalDistance += dist;
-
-            // Azuriraj proslu poziciju za sledeci krug
-            lastPlayerPos = playerPos;
-        }
-        else {
-            // Rezim merenja: Kamera je visoko (pticija perspektiva)
-            // Ovde ne menjamo cameraPos automatski, nego ga kontrolisu strelice u processInput
-            cameraPos.y = 10.0f; // Fiksna visina za mapu
-            view = glm::lookAt(cameraPos, cameraPos + glm::vec3(0.0f, -1.0f, 0.0f), cameraUp);
-        }
+        // View matrica (ona tvoja zakošena logika)
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, glm::vec3(0.0f, 1.0f, 0.0f));
 
         phongShader.setMat4("uP", projection);
         phongShader.setMat4("uV", view);
@@ -194,36 +210,100 @@ int main()
         glBindTexture(GL_TEXTURE_2D, mapTexture);
 
         glBindVertexArray(mapVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 6); // Crtamo mapu SAMO JEDNOM ovde
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        // --- C) Iscrtavanje LIKA (Kocke/Modela) ---
-        if (isWalkingMode) {
-            glm::mat4 model = glm::mat4(1.0f);
 
-            // 1. Postavi lika na njegovu poziciju
+        // --- C) SPECIFIČNO PO REŽIMIMA ---
+
+        if (isWalkingMode)
+        {
+            // --- Logika setanja ---
+            // Racunamo predjeni put
+            float dist = glm::distance(playerPos, lastPlayerPos);
+            totalDistance += dist;
+            lastPlayerPos = playerPos;
+
+            // --- Iscrtavanje LIKA ---
+            model = glm::mat4(1.0f);
             model = glm::translate(model, playerPos);
-
-            // 2. Rotiraj ga da gleda gde se krece
             model = glm::rotate(model, glm::radians(playerRotation), glm::vec3(0.0f, 1.0f, 0.0f));
-
-            // 3. Skaliraj ga (zadrzi ono sto smo nasli da radi, npr 0.05f)
-            model = glm::scale(model, glm::vec3(0.01f));
+            model = glm::scale(model, glm::vec3(0.01f)); // Pazi na skalu
 
             phongShader.setMat4("uM", model);
 
-            // Resetuj materijal na belo da se vidi originalna boja teksture
+            // Materijal i tekstura lika
             phongShader.setVec3("uMaterial.kA", 1.0f, 1.0f, 1.0f);
             phongShader.setVec3("uMaterial.kD", 1.0f, 1.0f, 1.0f);
             phongShader.setVec3("uMaterial.kS", 0.5f, 0.5f, 0.5f);
 
-            phongShader.setInt("uUseTexture", 1);
+            phongShader.setInt("uUseTexture", 1); // Lik koristi teksturu
             humanoidModel.Draw(phongShader);
+
         }
-        else {
-            // Rezim merenja: Kamera je visoko (pticija perspektiva)
-            // Ovde ne menjamo cameraPos automatski, nego ga kontrolisu strelice u processInput
-            cameraPos.y = 10.0f; // Fiksna visina za mapu
-            view = glm::lookAt(cameraPos, cameraPos + glm::vec3(0.0f, -1.0f, 0.0f), cameraUp);
+        else
+        {
+            // --- Logika merenja ---
+            // U ovom rezimu ne crtamo lika, vec ciode i linije
+
+            // 1. Iscrtavanje ČIODA
+            phongShader.setInt("uUseTexture", 0); // Iskljuci teksturu (bojimo rucno)
+
+            // Crvena boja
+            phongShader.setVec3("uMaterial.kD", 1.0f, 0.0f, 0.0f);
+            phongShader.setVec3("uMaterial.kS", 1.0f, 1.0f, 1.0f);
+            phongShader.setFloat("uMaterial.shine", 64.0f);
+
+            for (unsigned int i = 0; i < measurementPoints.size(); i++)
+            {
+                glm::mat4 model = glm::mat4(1.0f);
+
+                // 1. Postavi na tacnu lokaciju klika
+                model = glm::translate(model, measurementPoints[i]);
+
+                // --- NOVO: PODIZANJE MODELA ---
+                // Dodajemo ofset po Y osi da bi "igla" bola mapu, a lopta bila gore.
+                // Vrednost 1.0f ili 2.0f zavisi od toga koliko je model originalno velik.
+                // Probaj redom: 0.5f, 1.0f, 2.0f dok ne izgleda dobro.
+                model = glm::translate(model, glm::vec3(0.0f, 0.75f, 0.0f));
+
+                // 2. Skaliranje
+                model = glm::scale(model, glm::vec3(0.2f));
+
+                phongShader.setMat4("uM", model);
+                pinModel.Draw(phongShader);
+            }
+
+            // 2. Iscrtavanje LINIJA
+            if (measurementPoints.size() > 1)
+            {
+                // Boja: Koristimo Ambijentalnu komponentu da linija bude skroz CRVENA
+                // Posto linije nemaju normale, difuzno i spekularno mora biti 0
+                phongShader.setVec3("uMaterial.kA", 1.0f, 0.0f, 0.0f); // Crvena boja
+                phongShader.setVec3("uMaterial.kD", 0.0f, 0.0f, 0.0f); // Iskljuci difuzno
+                phongShader.setVec3("uMaterial.kS", 0.0f, 0.0f, 0.0f); // Iskljuci odsjaj
+
+                glm::mat4 model = glm::mat4(1.0f);
+
+                // KLJUČNO: Podižemo linije malo iznad poda (0.05 po Y osi)
+                // Ovo rešava problem "isprekidanih" linija (Z-fighting)
+                model = glm::translate(model, glm::vec3(0.0f, 0.05f, 0.0f));
+
+                phongShader.setMat4("uM", model);
+
+                // Ažuriramo podatke
+                glBindBuffer(GL_ARRAY_BUFFER, lineVBO);
+                glBufferData(GL_ARRAY_BUFFER, measurementPoints.size() * sizeof(glm::vec3), &measurementPoints[0], GL_DYNAMIC_DRAW);
+
+                glBindVertexArray(lineVAO);
+
+                // Povećavamo debljinu linije
+                glLineWidth(5.0f); // Probaj vrednost 3.0f ili 5.0f
+
+                glDrawArrays(GL_LINE_STRIP, 0, measurementPoints.size());
+
+                // Vraćamo debljinu na normalu da ne utiče na ostale objekte (ako crtamo wireframe)
+                glLineWidth(1.0f);
+            }
         }
 
 
@@ -234,18 +314,25 @@ int main()
         glEnable(GL_BLEND);
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        // Pazi: RenderText funkcija obicno sama radi UseProgram, ali nije na odmet
         glUseProgram(uiShader.ID);
 
         glm::mat4 uiProjection = glm::ortho(0.0f, (float)SCR_WIDTH, 0.0f, (float)SCR_HEIGHT);
         glUniformMatrix4fv(glGetUniformLocation(uiShader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(uiProjection));
 
+        // Prikaz teksta zavisno od rezima
         std::stringstream ss;
-        // Formatiramo broj na 2 decimale (npr. 12.45 m)
-        ss << "Ukupna predjena distanca: " << std::fixed << totalDistance;
+        if (isWalkingMode) {
+            ss << "Ukupna predjena distanca: " << std::fixed << totalDistance << "m";
+            // Ispis moda
+            RenderText(uiShader.ID, "Rezim: SETANJE", 25.0f, 25.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+        }
+        else {
+            ss << "Izmereno: " << std::fixed  << measurementTotalLen << "m";
+            // Ispis moda
+            RenderText(uiShader.ID, "Rezim: MERENJE", 25.0f, 25.0f, 1.0f, 1.0f, 1.0f, 1.0f);
+        }
 
-        // Ispisujemo gore levo (X=25, Y=Visina-50)
-        // Boja: Zuta (1.0, 1.0, 0.0) da se razlikuje
+        // Glavni ispis (Gore Levo) - Zuta boja
         RenderText(uiShader.ID, ss.str(), 25.0f, SCR_HEIGHT - 50.0f, 1.0f, 1.0f, 1.0f, 0.0f);
 
         // Reset za sledeci krug
@@ -285,7 +372,7 @@ void processInput(GLFWwindow* window)
         else {
             cameraPos.y = 10.0f; // Visoko za merenje
             // Gleda skoro skroz dole (kao 2D)
-            cameraFront = glm::vec3(0.0f, -0.99f, -0.1f);
+            cameraFront = glm::normalize(glm::vec3(0.0f, -1.0f, -1.0f));
         }
     }
     if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_RELEASE) {
@@ -356,4 +443,104 @@ void processInput(GLFWwindow* window)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    // Reagujemo na klik samo ako NIJE walking mode
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS && !isWalkingMode)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+
+        int width, height;
+        glfwGetWindowSize(window, &width, &height);
+
+        // ---------------------------------------------------------
+        // KORAK 1: REKONSTRUKCIJA MATRICA (Moraju biti ISTE kao u main-u)
+        // ---------------------------------------------------------
+
+        // A) Projekcija: 45 stepeni FOV, Aspect ratio prozora
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+        // B) View: Kamera pozicija i smer
+        // cameraFront je globalna promenljiva koja se azurira u processInput. 
+        // Ovim osiguravamo da klik "gleda" u istom pravcu kao i kamera.
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, glm::vec3(0.0f, 1.0f, 0.0f));
+
+        // ---------------------------------------------------------
+        // KORAK 2: PRETVARANJE 2D KLIKA U 3D ZRAK (Ray)
+        // ---------------------------------------------------------
+
+        // Viewport definise velicinu ekrana (x, y, sirina, visina)
+        glm::vec4 viewport = glm::vec4(0, 0, width, height);
+
+        // OpenGL krece od dole-levo, a Windows od gore-levo. Zato (height - ypos).
+        // Trazimo dve tacke u 3D prostoru koje se poklapaju sa tim pikselom:
+        // winPosNear -> Tacka na samom ekranu (Z=0.0)
+        // winPosFar  -> Tacka u beskonacnosti (Z=1.0)
+
+        glm::vec3 winPosNear = glm::vec3(xpos, height - ypos, 0.0f);
+        glm::vec3 winPosFar = glm::vec3(xpos, height - ypos, 1.0f);
+
+        // glm::unProject radi "magiju" - vraca 3D koordinate
+        glm::vec3 rayStart = glm::unProject(winPosNear, view, projection, viewport);
+        glm::vec3 rayEnd = glm::unProject(winPosFar, view, projection, viewport);
+
+        // Smer zraka je vektor od pocetka ka kraju
+        glm::vec3 rayDir = glm::normalize(rayEnd - rayStart);
+
+        // ---------------------------------------------------------
+        // KORAK 3: GDE ZRAK UDARA U POD (Y=0)?
+        // ---------------------------------------------------------
+
+        // Jednacina zraka: P = Start + Dir * t
+        // Nama treba P.y = 0 (Visina poda)
+        // 0 = Start.y + Dir.y * t
+        // -Start.y = Dir.y * t
+        // t = -Start.y / Dir.y
+
+        if (rayDir.y != 0.0f) // Zastita od deljenja sa nulom (ako gledamo skroz horizontalno)
+        {
+            float t = -rayStart.y / rayDir.y;
+
+            // t > 0 znaci da je presek ispred kamere (a ne iza ledja)
+            if (t > 0.0f)
+            {
+                // Konacna tacka preseka
+                glm::vec3 hitPoint = rayStart + rayDir * t;
+
+                // ---------------------------------------------------------
+                // KORAK 4: LOGIKA IGRE (Dodaj/Brisi ciodu)
+                // ---------------------------------------------------------
+
+                // Provera granica mape (-10 do 10)
+                if (hitPoint.x >= -10.0f && hitPoint.x <= 10.0f && hitPoint.z >= -10.0f && hitPoint.z <= 10.0f)
+                {
+                    bool deleted = false;
+                    // Provera za brisanje (da li smo kliknuli blizu postojece)
+                    for (int i = 0; i < measurementPoints.size(); i++) {
+                        if (glm::distance(measurementPoints[i], hitPoint) < 0.5f) { // Radijus 0.5
+                            measurementPoints.erase(measurementPoints.begin() + i);
+                            deleted = true;
+                            break;
+                        }
+                    }
+
+                    // Ako nismo obrisali, dodaj novu
+                    if (!deleted) {
+                        measurementPoints.push_back(hitPoint);
+                    }
+
+                    // Ponovo izracunaj ukupnu duzinu
+                    measurementTotalLen = 0.0f;
+                    if (measurementPoints.size() > 1) {
+                        for (int i = 0; i < measurementPoints.size() - 1; i++) {
+                            measurementTotalLen += glm::distance(measurementPoints[i], measurementPoints[i + 1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
